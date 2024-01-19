054cd24c770c9a8b07a59eb053eed13063d2893c
NutchanonPh <nutchanon.ph@cs.rmu.ac.th>
2024-01-19 16:01:23 +0700

    express + lab

--- Express/index.js
+++ Express/index.js
@@ -15,10 +15,38 @@ const app = express();
 // app.patch("/", (req, res)=> {});
 // app.delete("/", (req, res)=> {});
 
+app.listen("8000", ()=> {
+    console.log("Server is running on port 8000")
+});
+
+// Lab 1 Express
 app.get("/", (req, res)=> {
     res.json({title: "My First Web App", message: "Our Web App API"})
 });
 
-app.listen("8000", ()=> {
-    console.log("Server is running on port 8000")
+//Lab 2 Express
+app.get("/post", (req, res)=> {
+    const method = req.method
+    res.json({message: method + " " + "products"});
+});
+app.post("/post", (req, res)=> {
+    const method = req.method
+    res.json({message: method + " " + "products"});
+});
+app.put("/post", (req, res)=> {
+    const method = req.method
+    res.json({message: method + " " + "products"});
+});
+app.patch("/post", (req, res)=> {
+    const method = req.method
+    res.json({message: method + " " + "products"});
+});
+app.delete("/post", (req, res)=> {
+    const method = req.method
+    res.json({message: method + " " + "products"});
+});
+
+//Lab 3 Express
+app.get("/redirect", (req, res)=> {
+    res.redirect("https://google.com")
 });
