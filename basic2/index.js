bcf33bfd6d7661c351f2536ff99f874ca5b71572
NutchanonPh <nutchanon.ph@cs.rmu.ac.th>
2024-01-19 13:42:47 +0700

    Backend 2

--- basic2/index.js
+++ basic2/index.js
@@ -18,7 +18,41 @@
 // console.log(__dirname);
 // console.log(__filename);
 
-const path = require("path");
+// const path = require("path");
 
-console.log(__dirname + "/" + "data.js");
-console.log(path.join(__dirname, "data.js"));
+// console.log(__dirname + "/" + "data.js");
+// console.log(path.join(__dirname, "data.js"));
+
+//ReadFile and WriteFile
+
+// const fs = require("fs");
+// const path = require("path");
+// fs.readFile(
+//     path.join(__dirname, "hello.txt"), 
+//     {encoding: "utf-8"},
+//     (err, data)=> {
+//         // console.log(data);
+//         //WriteFile
+//         fs.writeFile("hello2.txt", data, ()=> {
+//             console.log("Done WriteFile")
+//         })
+// });
+
+// const fs = require("fs/promises");
+// const path = require("path");
+// const run = async ()=> {
+//     const data = await fs.readFile(path.join(__dirname, "hello.txt"), {
+//         encoding: "utf-8",
+//     });
+//     console.log(data);
+// };
+// run();
+
+// Use library http
+const http = require("http");
+
+const server = http.createServer((req, res)=> {});
+
+server.listen("8000", ()=> {
+    console.log("Server is running on port 8000")
+});
