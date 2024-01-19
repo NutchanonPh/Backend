38d6e39cdd840252bb4576969594b480f1989f28
NutchanonPh <nutchanon.ph@cs.rmu.ac.th>
2024-01-19 12:00:09 +0700

    Backend 1

---
+++ basic2/index.js
@@ -0,0 +1,24 @@
+// const obj = require("./data")
+
+//const testFunc = require("./data");
+
+// console.log(obj)
+
+//const {a, testFunc} = require("./data");
+
+// console.log(a);
+// console.log(testFunc);
+
+// const helloFunc = require("./data");
+// console.log(helloFunc());
+
+// const {testFunc} = require("./data");
+// testFunc;
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const path = require("path");
+
+console.log(__dirname + "/" + "data.js");
+console.log(path.join(__dirname, "data.js"));
