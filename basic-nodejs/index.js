26e97d5cd559339a7f2bc91c0ee73841a7c90eee
NutchanonPh <nutchanon.ph@cs.rmu.ac.th>
2024-01-22 11:15:05 +0700

    Backend 3

--- basic-nodejs/index.js
+++ basic-nodejs/index.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const app = express();
-
 // const bodyMiddleware = express.json();
 // app.use(express.json());
 
@@ -10,23 +7,48 @@ const app = express();
 //     res.json({username, password});
 // });
 
-const middlewareA = (req, res, next) => {
-    const {message} = req.body;
-    req.myMessage = message;
-    next();
-};
+// const middlewareA = (req, res, next) => {
+//     const {message} = req.body;
+//     req.myMessage = message;
+//     next();
+// };
 
 //app.use(middlewareA);
 
-app.get("/", (req, res, next)=> {
-    console.log(req.myMessage);
-    res.json();
-});
+// app.get("/", (req, res, next)=> {
+//     console.log(req.myMessage);
+//     res.json();
+// });
 
-app.patch("/", middlewareA, (req, res, next)=> {
-    console.log(req.myMessage);
-    res.json();
-})
+// app.patch("/", middlewareA, (req, res, next)=> {
+//     console.log(req.myMessage);
+//     res.json();
+// })
+
+const express = require("express");
+const multer = require("multer");
+
+const upload = multer({dest: 'uploads/'})
+const app = express();
+
+app.use(express.json());
+
+app.post("/", upload.single("image"), (req, res, next)=> {
+    try {
+    console.log(req.file);
+    // upload ขึ้น cloud
+    // return url
+    // update url => db server
+    // delete file => fs.unlinkSync
+    res.json({});
+    } catch(err) {
+        next(err);
+    } finally {
+        setTimeout(()=> {
+            fs.unlinkSync(req.file.path);
+        }, 3000)
+    }
+});
 
 app.listen("8000", ()=> {
     console.log("Server is running on port 8000");
